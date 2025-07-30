4455f392a71376d07d1d4f9c587a1a6eedfe89f6
ankur0704 <sonavaneankur@gmail.com>
2025-07-30 21:28:35 +0530

    list

--- C++STL.cpp
+++ C++STL.cpp
@@ -14,3 +14,18 @@ int main(){
     int s=sum(1,5);
     return 0;
 }
+
+//vectors
+void explainVector(){
+    
+}
+
+//List
+void explainList(){
+    list<int>ls;
+
+    ls.push_back(2); //{2}
+    ls.emplace_back(4); //{2,4}
+    ls.push_front(5);
+    ls.emplace_front();
+}
