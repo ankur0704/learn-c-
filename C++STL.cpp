16ba04c61cd8d610216db5b81c3c7eca16dec905
ankur0704 <sonavaneankur@gmail.com>
2025-07-30 15:26:14 +0530

    stlC++,functions

---
+++ C++STL.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(){   //void function
+    cout<<"ankur";
+}
+
+int sum(int a , int b){   //return type function
+    return a+b;
+}
+
+int main(){
+    print();
+    int s=sum(1,5);
+    return 0;
+}
