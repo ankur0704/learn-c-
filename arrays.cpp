54157525e3f5f61e228487563dd3da94ee2e021a
ankur0704 <sonavaneankur@gmail.com>
2025-08-03 21:12:37 +0530

    array-day3-(missing no and consectuitive of ones)

--- arrays.cpp
+++ arrays.cpp
@@ -11,9 +11,38 @@ using namespace std;
 //linear search
 // ex:-arr[] =[1,3,45,1,3,6]  num=4
 
-for(int i=0; i<n; i++){
-    if(arr[i]==num){
-        return i;
+// for(int i=0; i<n; i++){
+//     if(arr[i]==num){
+//         return i;
+//     }
+//     return -1;  
+// }
+
+int findMaximumConsecutivesOfOnes(vector<int>& nums){
+    int maxi=0;
+    int cnt=0;
+    
+    for(int i =0; i<nums.size(); i++){
+        if(nums[i]==1){
+            cnt++;
+            maxi= max(maxi, cnt);
+
+        }else{
+            cnt=0;
+        }
     }
-    return -1;  
-}
+    return maxi;
+
+
+};
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int XOR=0;
+        for(int i=0; i<nums.size();i++){
+            XOR=  XOR^nums[i];
+        }
+        return XOR;
+        
+    }
+};
