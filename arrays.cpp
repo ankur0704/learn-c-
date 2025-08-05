4f06efa6badaa0489372307d1f196118d48a451f
ankur0704 <sonavaneankur@gmail.com>
2025-08-05 21:37:37 +0530

    hashing(number, character)

--- arrays.cpp
+++ arrays.cpp
@@ -152,3 +152,58 @@ void f(int arr, int i, int n){
         for(int i=0; i<n;i++) cout<< arr[i]<<" ";
         return 0;
     }
+
+    // HASHING
+
+    int main(){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++){
+            cin>> arr[i];
+        }
+
+        //precompute 
+        int hash[13]={0};
+        for(int i=0; i<n; i++){
+            hash[arr[i]] += 1;
+        }
+
+
+        int q;
+        cin>> q;
+        while(q--){
+            int number;
+            cin>> number;
+            //fetch method
+            cout<< hash[number]<< endl; 
+
+        }
+
+        return 0;
+    }
+
+
+
+//character hashing
+int main(){
+    string s;
+    cin>> s;
+
+    //pre compute
+    int hash[26]={0};
+    for(int i=0; i<s.size();i++){
+        hash[s[i]]++;
+    }
+
+    int q;
+    cin>> q;
+    while(q--){
+        char c;
+        cin>> c;
+
+        //fetch
+        cout<< hash[c-'a']<<endl;
+    }
+    return 0;
+}
