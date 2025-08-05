f84a53db467f86046dcf6f79edb6b198f66652fb
ankur0704 <sonavaneankur@gmail.com>
2025-08-05 20:44:49 +0530

    swap of array without using 2 pointer

--- arrays.cpp
+++ arrays.cpp
@@ -129,3 +129,26 @@ int main(){
     cout<<fact<<(n);
     return 1;
 }
+
+// swap of array without using 2 pointer
+void f(int arr, int i, int n){
+
+    if(i>=n/2) return;
+    
+    swap(arr[i], arr[n-i-1]);
+
+    f(i+1, ar,n);
+
+
+    }
+
+    int main(){
+    
+        int n;
+        cin>> n;
+        int arr[n];
+        for(int i=0; i <n;i++) cin>> arr[i];
+        f(0,arr,n);
+        for(int i=0; i<n;i++) cout<< arr[i]<<" ";
+        return 0;
+    }
