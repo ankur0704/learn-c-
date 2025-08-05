c1bf07e1617726fd89b4164ad1c6e045024df20f
ankur0704 <sonavaneankur@gmail.com>
2025-08-05 12:00:29 +0530

    sum of first n nos.

--- arrays.cpp
+++ arrays.cpp
@@ -104,3 +104,17 @@ return "NO";
     func(1,n)
     return 0;
  }
+
+ //backtracking in nb
+
+ //SUM OF FIRST N NOS. using functional way
+ int sum(int n){
+    if(n==0) return 0;
+    return n+sum(n-1);
+ }
+ int main(){
+    int n =3;
+    cout<<sum(n);
+
+    return 0;
+ }
