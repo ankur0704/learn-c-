c321c5e78c7b15127ee92672ea8d2babc42fb1a8
ankur0704 <sonavaneankur@gmail.com>
2025-08-06 18:26:53 +0530

    selection sort

--- arrays.cpp
+++ arrays.cpp
@@ -245,3 +245,31 @@ void freq(int arr[], int n){
         }
     }
 }
+
+void selection_sort(int arr[], int n){
+    for(int i=0; i<n-2; i++){
+        int mini=1;
+        for(int j=i;j<=n-1;j++){
+            if(arr[j]<arr[mini]){
+                mini=j;
+            }
+        }
+        int temp= arr[mini];
+        arr[mini]=arr[i];
+        arr[i]=temp;
+        }
+
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++) cin>> arr[i];
+    selection_sort(arr,n);
+    for(int i=0; i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
