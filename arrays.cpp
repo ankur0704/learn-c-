55a157c0055732898151a7c7aadd3c1c63167da3
ankur0704 <sonavaneankur@gmail.com>
2025-08-07 21:23:25 +0530

    subarrays(leetcode 53)

--- arrays.cpp
+++ arrays.cpp
@@ -421,3 +421,43 @@ void sortCOlor(vector<int>&nums){
         }
     }
 }
+   
+
+
+
+//subarrays
+int main(){
+    int n=5;
+    int arr[5]={1,2,3,4,5};
+
+    for(int st=0; st<n; st++){
+        for(int end=st; end<n; end++){
+            for(int i=st; i<=end;i++){
+                cout<<arr[i];
+            }
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+//brute force for maximum subarray sum
+int main(){
+    int n=5;
+    int arr[5]={1,2,3,4,5};
+
+    int maxSum=INT_MIN;
+
+    for(int st=0; st<n; st++){
+        int currSum=0;
+        for(int end=st; end<n; end++){
+            currSum+= arr[end];
+            maxSum=max(currSum, maxSum);
+        }
+        
+    }
+    cout<<"max subarray sum="<< maxSum<<endl;
+    return 0;
+}
+
