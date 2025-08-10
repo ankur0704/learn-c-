25001cdb158d5a453860e7621c2b20bf55720954
ankur0704 <sonavaneankur@gmail.com>
2025-08-11 02:14:21 +0530

    new file oops

---
+++ oops.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Teacher{
+    //properties / attributes
+    string name;
+    string dept;
+
+    //methods / member functions
+    void changeDept(string newDept){
+        dept=newDept;
+    }
+
+};
+
+int main(){
+    Teacher t1;// t1 is object
+    Teacher t2;
+    Teacher t3;
+}
