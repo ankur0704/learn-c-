051c3fce50ea9435e50f9b6b90c802264fd7d50f
ankur0704 <sonavaneankur@gmail.com>
2025-08-11 18:34:11 +0530

    constructors

--- oops.cpp
+++ oops.cpp
@@ -2,19 +2,64 @@
 using namespace std;
 
 class Teacher{
+
+    public:
+    //non parametrized constructor
+    Teacher(){  //a constructor created by devloper itsself. 
+        cout<<"Hi i am constructor\n";
+    }
+
+    //parametrized
+    Teacher(string n, string d, string s, double sal){
+        name=n;
+        dept=d;
+        subj=s;
+        salary=sal;
+    }
+
+
     //properties / attributes
     string name;
     string dept;
+    double salary;
+    string subj;
 
     //methods / member functions
     void changeDept(string newDept){
         dept=newDept;
     }
+    //setter
+    void setSalary(double s){
+        salary=s;
+    }
+    //getter
+    double getSalary(){
+        return salary;
+    }
 
 };
 
+class Acount{
+    private:
+    double balance;
+    string password;  //data hiding
+    
+    public:
+    string accountId;
+    string username;
+    
+};
+
+
 int main(){
-    Teacher t1;// t1 is object
+    Teacher
+    Teacher t1; //constrictor is called // t1 is object
+    t1.name="Ankur";
+    t1.subj="Maths";
+    t1.setSalary=(5000);
     Teacher t2;
     Teacher t3;
+
+    cout<<t1.getSalary()<<endl
+    return 0;
 }
