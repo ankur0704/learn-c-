5a567bd0b0049e620441727ec34010805140007c
ankur0704 <sonavaneankur@gmail.com>
2025-08-12 20:11:00 +0530

    Abstraction(abstract class,etc)

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -3,21 +3,48 @@
 #include<string>
 using namespace std;
 
-class Student{
+// class Student{
+//     public:
+//     string name;
+
+//     Student(){
+//         cout<<"non-parametrized constructor";
+
+//     }
+//     Student(string name){
+//         this->name=name;
+//         cout<<"parametrized constructor";
+//     }
+// };
+
+// class Print{  //function overloading ex:
+//     public:
+//     void show(int x){
+//         cout<<"int :"<<x<<endl;
+//     }
+
+//     void show(char ch){
+//         cout<<"char :"<<ch<<endl;
+//     }
+// };
+
+//abstract class
+class Shape{    //abstract class
+    virtual void draw()=0;  //pure virtual function
+};
+
+class Circle : public Shape{
     public:
-    string name;
-
-    Student(){
-        cout<<"non-parametrized constructor";
-
-    }
-    Student(string name){
-        this->name=name;
-        cout<<"parametrized constructor";
+    void draw(){
+        cout<<"drawing a circle\n";
     }
 };
 
 int main(){
-    Student s1;
+    Circle c1;
+    c1.draw();
+    // Printp1;
+    // p1.show('&');
+    // Student s1;
     return 0;
 }
