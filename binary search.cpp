e765a8de0e3efc705aa13c1b6980fafd5d087871
ankur0704 <sonavaneankur@gmail.com>
2025-08-13 22:01:58 +0530

    binary search(iterative code )

--- binary search.cpp
+++ binary search.cpp
@@ -1,1 +1,17 @@
 #include<bits/stdc++.h>
+//iterative code for search in binary search
+
+int search(vector<int>& nums, int target){
+
+    int n=nums.size();
+    int low=0, high=n-1;
+
+    while(low<=high){
+        int mid= (low+high)/2;
+        if(nums[mid]==target)  return mid;
+
+        else if(target>nums[mid]) low= mid+1;
+        else high = mid-1;
+    }
+    return -1;
+}
