9894d5388465be91c910f90fdc4a1ef64458cd53
ankur0704 <sonavaneankur@gmail.com>
2025-08-14 14:03:14 +0530

    binary search(lower and upper bound)

--- binary search.cpp
+++ binary search.cpp
@@ -15,3 +15,42 @@ int search(vector<int>& nums, int target){
     }
     return -1;
 }
+
+//lower bound
+int lowerbound(vector<int>arr, int n, int x){
+    int low= 0, high=n-1;
+    int ans = n;
+    while(low<=high){
+        int mid=(low+high)/2;
+        //may be ans
+        if(arr[mid]>=x){
+            ans=mid;
+            // look for small index in mid
+            high = mid-1;
+        }
+        else{
+            low=mid+1; //look for right
+        }
+    }
+    return ans;
+}  //tc-> 0(log n)
+
+
+//upper bound
+int lowerbound(vector<int>arr, int n, int x){
+    int low= 0, high=n-1;
+    int ans = n;
+    while(low<=high){
+        int mid=(low+high)/2;
+        //may be ans
+        if(arr[mid]>x){
+            ans=mid;
+            // look for small index in mid
+            high = mid-1;
+        }
+        else{
+            low=mid+1; //look for right
+        }
+    }
+    return ans;
+}  //tc-> 0(log n)
