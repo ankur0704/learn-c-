2f805c6b652c6b78213117cad038178f427ac245
ankur0704 <sonavaneankur@gmail.com>
2025-08-17 19:50:46 +0530

    BS_problem

--- binary search.cpp
+++ binary search.cpp
@@ -116,3 +116,31 @@ public:
         
     // }   TC:-//O(N)
 
+
+    //find peak element 
+   class Solution {
+public:
+    int findPeakElement(vector<int>& arr) {
+        int n= arr.size();
+        if(n==1) return 0;
+        if(arr[0]>arr[1]) return 0;
+        if(arr[n-1]> arr[n-2]) return n-1;
+
+        int low=1, high= n-2;
+        while(arr[low]<= arr[high]){            //when submitting in leetcode showing wrong ans(a bug), instead do "while(low<= high)"
+            int mid=(low+high)/2;
+            //If arr[mid] is the peak:
+            if(arr[mid]> arr[mid-1] && arr[mid]> arr[mid+1]){
+                return mid;
+                   // If we are in the left:
+            }else if(arr[mid]> arr[mid-1])low= mid+1;
+            // If we are in the right:
+            // Or, arr[mid] is a common point:
+            else high = mid-1;
+        }
+        // Dummy return statement
+        return -1;
+        
+    }
+};   //tc - O(logN)
+
