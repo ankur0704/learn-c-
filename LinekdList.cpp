de8caff9d412dccfe2931cbb072ee45f6006826e
ankur0704 <sonavaneankur@gmail.com>
2025-08-31 20:12:05 +0530

    LL(traversal, search,length)

--- LinekdList.cpp
+++ LinekdList.cpp
@@ -14,9 +14,49 @@ struct Node{
     }
 };
 
+
+Node* converArrToLL(vector<int> &arr){
+    Node* head= new Node(arr[0]);
+    Node* mover = head;
+    for(int i=1; i<arr.size(); i++){
+        Node* temp= new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+int lengthOfLL(Node* head){
+    int cnt=0;
+    Node* temp=head;
+    while(temp){
+        cout<< temp-> data<<" ";
+        temp= temp-> next;
+        cnt++;    
+    }
+return cnt;
+}
+
+int checkIfPresent(Node* head, int val){
+    Node* temp= head;
+    while(temp){
+        if(temp-> data==val) return 1;
+        temp= temp-> next;
+    }
+    return 0;
+}
+
+
 int main(){                                          //
     vector<int> arr={1,5,8,7};
-    Node* y= new Node(arr[0], nullptr);
-    cout<<y; 
-
+    Node* head= converArrToLL(arr);
+    cout<<head->data;
+    // Node* y= new Node(arr[0], nullptr);
+    // cout<<y; 
+    cout< checkIfPresent(head, 15);
+    //traversal in LL
+    Node* temp=head;
+    while(temp){
+        cout<< temp-> data<<" ";
+        temp= temp-> next;
+    }
 }
