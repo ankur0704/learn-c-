cc3c398919282b348a14c28efd41be92dbe23c01
ankur0704 <sonavaneankur@gmail.com>
2025-09-29 21:08:58 +0530

    leetcode 75

--- arrays.cpp
+++ arrays.cpp
@@ -945,6 +945,37 @@ int main(){
 };
 
 
+//leetcode problem 75
 
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n= nums.size();
+        int index=0;
+        int left= 0;
+        int right = n-1;
+
+
+        while(index <= right){
+            if(nums[index]==0){
+                swap(nums[index], nums[left]);
+                left++;
+                index++;
+            }
+            else if(nums[index] ==2){
+                swap(nums[index], nums[right]);
+                    right--;
+                    //catch-> no idea of index++
+                    //index++
+                 
+            }
+            else{
+                index++;
+            }
+
+
+        }
+    }
+};
 
 
