49a7bd16fa547121e3e5314bcffa8fcaee543c98
ankur0704 <sonavaneankur@gmail.com>
2025-09-29 14:20:57 +0530

    array(solved questions, call by value, call by reference)

--- arrays.cpp
+++ arrays.cpp
@@ -780,3 +780,136 @@ int main(){
     extremePrint(arr, size);
 };
 
+
+
+// Find unique element
+int getUnique(int arr[], int n){
+    int ans = 0;
+    for(int i=0; i<n; i++){
+        ans = ans^arr[i];
+    }
+    return ans;
+
+}
+    
+int main(){
+    int arr[]= {2,10,11,10,2,13,15,13,15};
+    int n = 9;
+
+    int findAns = getUnique(arr, n);
+    cout<<"Final answer is:"<<finalAns<<endl;
+};
+
+
+
+//creating pairs
+
+int main(){
+    int arr[]={10,20,30};
+    int n=3;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; i<n; i++){
+            cout<<arr[i]<<", "<<arr[j]<<endl;
+        }
+    }
+}
+
+//print triplet
+
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n=5;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            for(int k=0; k<n; k++){
+                cout<<arr[i]<<" "<<arr[j]<<" "<<arr[k]<<endl;
+            }
+        }
+    }
+}
+
+
+//
+
+
+void sortZeroOne(int arr[], int n){
+    int zeroCount = 0;
+    int oneCount = 0;
+
+    //set1 count zero and 1
+    for(int i=0; i<n; i++){
+        if(arr[i] == 0){
+            zeroCount++;
+        if(arr[i] == 1){
+            oneCount++;
+        }
+        }
+        //step2 place all zero first
+        int i;
+        for(int i=0; i<zeroCount; i++){
+            arr[i]=0;
+        }
+        // step3 place all ones first
+        for(int j=i; j<n; j++){
+            arr[j] = 1;
+        }
+    }
+
+    //EASY WAY
+    int index=0;
+
+    while(zeroCount--){
+        arr[index] = 0;
+        index++;
+    }
+    while(oneCount--){
+        arr[index] = 1;
+        index++;
+    }
+
+}
+
+
+int main(){
+    int arr[]={0,1,0,1,0,1,1,1,1,0,0,0,0,0};
+    int n=14;
+
+    sortZeroOne(arr, n);
+
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+};
+
+
+//shirt arrays elements right by 1(right shift)
+
+void shiftArray(int arr[], int n){
+    //step1
+    int temp = arr[n-1]
+
+    //step2 - 
+    for(int i= n-1; i>=1; i--){
+        arr[i] = arr[i-1];
+    }
+
+    //step2 copy temp into 0th index
+    arr[0] = temp;
+};
+
+
+int main(){
+    int arr[] = {10,20,30,40,50,60};
+    int n=6;
+
+    shiftArray(arr, n);          //created function shiftArray
+
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";               //printed shiftArray
+    }
+};
+
+
+
